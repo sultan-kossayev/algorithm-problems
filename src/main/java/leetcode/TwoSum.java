954a2359fa129455f2309831f5fc0415a7cc19f0
Sultan Kossayev <sultan.kossayev@gmail.com>
2019-08-31 13:48:04 +0300

    "#1. Two Sum" from leetcode

--- src/main/java/leetcode/TwoSum.java
+++ src/main/java/leetcode/TwoSum.java
@@ -8,31 +8,31 @@ import java.util.HashMap;
 //https://leetcode.com/problems/two-sum/
 public class TwoSum {
 
-	/**
-	 * Using a hash map for locating a complement in one pass.
-	 *
-	 * Time: O(N)
-	 * Space: O(N)
-	 */
-	private static int[] twoSum(int[] nums, int target) {
-		Map<Integer, Integer> map = new HashMap<>();
+    /**
+     * Using a hash map for locating a complement in one pass.
+     *
+     * Time: O(N)
+     * Space: O(N)
+     */
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-		for (int i = 0;i < nums.length;i++) {
-			int complement = target - nums[i];
-			if (map.containsKey(complement)) {
-				return new int[] {map.get(complement), i};
-			}
+        for (int i = 0;i < nums.length;i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
 
-			map.put(nums[i], i);
-		}
+            map.put(nums[i], i);
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public static void main(String... args) {
-		int[] nums = new int[]{2,3,4,5};
-		int[] sum = twoSum(nums, 9);
+    public static void main(String... args) {
+        int[] nums = new int[]{2,3,4,5};
+        int[] sum = twoSum(nums, 9);
 
-		ArrayAssertions.assertEqual(sum, new int[]{2,3});
-	}
+        ArrayAssertions.assertEqual(sum, new int[]{2,3});
+    }
 }
