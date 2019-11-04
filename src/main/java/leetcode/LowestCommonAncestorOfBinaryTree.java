7a2fee49b0a0bb8cd4694c13e8ef95293992dd9a
Sultan Kossayev <sultan.kossayev@gmail.com>
2019-11-04 16:59:12 +0300

    "#236. Lowest Common Ancestor of a Binary Tree" from leetcode

--- src/main/java/leetcode/LowestCommonAncestorOfBinaryTree.java
+++ src/main/java/leetcode/LowestCommonAncestorOfBinaryTree.java
@@ -2,10 +2,15 @@ package leetcode;
 
 import utils.TreeBuilder.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 public class LowestCommonAncestorOfBinaryTree {
 
     /**
+     * Recursive approach.
+     *
      * Two cases we have to handle:
      * 1. if p and q sit in the same subtree of root, then ancestor will be p or q,
      * depending which we will reach first
@@ -33,5 +38,66 @@ public class LowestCommonAncestorOfBinaryTree {
 
         // ancestor is in left or right subtree of root
         return left != null ? left : right;
+    }
+
+    /**
+     * Iterative approach.
+     * The idea is to use postorder traversal to build a path to p and q nodes
+     * and the compare both paths and get the last common node.
+     * <p>
+     * Time: O(n)
+     * Space: O(n)
+     */
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = root;
+        TreeNode prev = null;
+
+        // path to p or q
+        Deque<TreeNode> path = null;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            TreeNode top = stack.peek();
+            if (top.right != null && top.right != prev) {
+                curr = top.right;
+            } else {
+                if (top == p || top == q) {
+                    if (path == null) {
+                        path = new LinkedList<>(stack);
+                    } else {
+                        // we already have path to p or q, then whatever we have in stack will serve as 2nd path for us
+                        break;
+                    }
+                    // edge case when p equals to q
+                    if (p == q) {
+                        break;
+                    }
+                }
+
+                prev = top;
+                stack.pop();
+            }
+        }
+
+        // get last common node
+        TreeNode common = null;
+        while (!path.isEmpty() && !stack.isEmpty()) {
+            TreeNode top = null;
+            if ((top = path.removeLast()) != stack.removeLast()) {
+                break;
+            }
+
+            common = top;
+        }
+
+        return common;
     }
 }
